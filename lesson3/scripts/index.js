0c6a83348d5b0b7c4bbae43fae5e4f7d81a73e13
ezzze28 <papitomiret@gmail.com>
2023-05-06 02:44:10 -0300

    fist commit

--- lesson3/scripts/index.js
+++ lesson3/scripts/index.js
@@ -8,4 +8,8 @@ function display(){
     let tbox = document.querySelector("#last_modified");
     let last = document.lastModified;
     tbox.innerHTML = last;
+
+    const currentYear = document.getElementById("currentYear")
+    date = new Date()
+    currentYear.innerText = date.getFullYear();
 }
