562e98a23711e10234939b9492fa65fec2c16dce
ezzze28 <papitomiret@gmail.com>
2024-01-13 07:55:53 -0300

    js pen

--- Bountiful Foods/Design/scripts/design.js
+++
@@ -1,8 +0,0 @@
-
-const hamburger = document.getElementById('hamburgerDesign');
-const menu = document.querySelector('.mainMenu');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  menu.classList.toggle('active');
-});
--- lesson7/scripts/script.js
+++
@@ -1,9 +0,0 @@
-//Current Date
-const currentYear = document.getElementById("currentYear")
-date = new Date()
-currentYear.innerText = date.getFullYear();
-
-//Last update
-const secondLineFooter = document.getElementById("secondLineFooter")
-
-secondLineFooter.innerText = `Last Update: ${document.lastModified}`
